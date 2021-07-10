61fded1626636261a79cf3ae79a131f66c3d0740
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-07-10 22:44:24 +0900

    feat : 회원가입, 수정, 탈퇴처리 로그 추가

--- src/main/java/nextstep/subway/member/ui/MemberController.java
+++ src/main/java/nextstep/subway/member/ui/MemberController.java
@@ -5,6 +5,8 @@ import nextstep.subway.auth.domain.LoginMember;
 import nextstep.subway.member.application.MemberService;
 import nextstep.subway.member.dto.MemberRequest;
 import nextstep.subway.member.dto.MemberResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,6 +14,9 @@ import java.net.URI;
 
 @RestController
 public class MemberController {
+
+    private static final Logger logger = LoggerFactory.getLogger(MemberController.class);
+
     private MemberService memberService;
 
     public MemberController(MemberService memberService) {
@@ -20,6 +25,7 @@ public class MemberController {
 
     @PostMapping("/members")
     public ResponseEntity createMember(@RequestBody MemberRequest request) {
+        logger.info("회원가입 요청 : " + request);
         MemberResponse member = memberService.createMember(request);
         return ResponseEntity.created(URI.create("/members/" + member.getId())).build();
     }
@@ -32,12 +38,15 @@ public class MemberController {
 
     @PutMapping("/members/{id}")
     public ResponseEntity<MemberResponse> updateMember(@PathVariable Long id, @RequestBody MemberRequest param) {
+        logger.info("updateMember id : " + id);
+        logger.info("updateMember content : " + param);
         memberService.updateMember(id, param);
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/members/{id}")
     public ResponseEntity<MemberResponse> deleteMember(@PathVariable Long id) {
+        logger.info("deleteMember id : " + id);
         memberService.deleteMember(id);
         return ResponseEntity.noContent().build();
     }
@@ -50,12 +59,15 @@ public class MemberController {
 
     @PutMapping("/members/me")
     public ResponseEntity<MemberResponse> updateMemberOfMine(@AuthenticationPrincipal LoginMember loginMember, @RequestBody MemberRequest param) {
+        logger.info("updateMemberOfMine id : " + loginMember.getId());
+        logger.info("updateMemberOfMine content : " + param);
         memberService.updateMember(loginMember.getId(), param);
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/members/me")
     public ResponseEntity<MemberResponse> deleteMemberOfMine(@AuthenticationPrincipal LoginMember loginMember) {
+        logger.info("deleteMemberOfMine id : " + loginMember.getId());
         memberService.deleteMember(loginMember.getId());
         return ResponseEntity.noContent().build();
     }
