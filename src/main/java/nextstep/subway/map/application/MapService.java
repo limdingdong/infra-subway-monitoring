067c243c7aeddcab2fc5e35633e7d78e911ec037
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-07-10 23:37:23 +0900

    feat : 경로조회 로그를 파일로그로 출력

--- src/main/java/nextstep/subway/map/application/MapService.java
+++ src/main/java/nextstep/subway/map/application/MapService.java
@@ -7,6 +7,8 @@ import nextstep.subway.map.dto.PathResponse;
 import nextstep.subway.map.dto.PathResponseAssembler;
 import nextstep.subway.station.application.StationService;
 import nextstep.subway.station.domain.Station;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,6 +17,9 @@ import java.util.List;
 @Service
 @Transactional
 public class MapService {
+
+    private static final Logger fileLogger = LoggerFactory.getLogger("file");
+
     private LineService lineService;
     private StationService stationService;
     private PathService pathService;
@@ -29,8 +34,9 @@ public class MapService {
         List<Line> lines = lineService.findLines();
         Station sourceStation = stationService.findById(source);
         Station targetStation = stationService.findById(target);
+        fileLogger.info("sourceStation : " + source);
+        fileLogger.info("targetStation : " + target);
         SubwayPath subwayPath = pathService.findPath(lines, sourceStation, targetStation);
-
         return PathResponseAssembler.assemble(subwayPath);
     }
 }
